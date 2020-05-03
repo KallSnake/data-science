8f331e77affc7527fd87cc05dcbadc7e3d121ba8
Luis Carlos da Silva <34668802+KallSnake@users.noreply.github.com>
2020-05-03 14:57:26 -0300

    Update Plots.R

--- Dataset/Dataset - Estatistica para Data Science/Plots.R
+++ Dataset/Dataset - Estatistica para Data Science/Plots.R
@@ -4,8 +4,7 @@
 
 
 # Definindo a pasta de trabalho
-# Substitua o caminho abaixo pela pasta no seu computador
-setwd("C:/Users/kblui/Downloads/DSA - Data Science Academy/Power BI para Data Science/Dataset/Dataset - Estatistica para Data Science")
+setwd("C:/Users/Data Science/Dataset/Dataset - Estatistica para Data Science")
 getwd()
 
 
